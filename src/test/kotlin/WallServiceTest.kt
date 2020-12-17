3a4139f0c4178ee84c7553920a494b207e68902f
Melkov Denis <d.a.melkov@gmail.com>
2020-12-17 22:59:07 +0300

    add empty tests

---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,16 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        fail("Empty test")
+    }
+
+    @Test
+    fun update() {
+        fail("Empty test")
+    }
+}
